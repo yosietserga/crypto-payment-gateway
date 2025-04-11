6e5eb4831c9bf7deaf38226fb24b64be86b6f406
Yosiet Serga <yosietserga@gmail.com>
2025-04-11 14:35:00 -0400

    Delete dist directory

--- dist/reflect-metadata.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// This file ensures reflect-metadata is imported once at the application entry point
-require("reflect-metadata");
-//# sourceMappingURL=reflect-metadata.js.map
